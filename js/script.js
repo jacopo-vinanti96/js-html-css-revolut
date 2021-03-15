ab384e99bac044e307974963075b4189fb5ca08d
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-15 17:22:58 +0100

    W.I.P.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 var dropdownLabels = $( '.dropdown-label' ),
-    dropdownLists = $('.dropdown-list')
-    documentHTML = $('');
+    dropdownLists = $('.dropdown-list'),
+    documentHTML = $(''),
+    clicked = false;
 
 // documentHTML.click( function () {
-//   if ( dropdownLabels.hasClass( 'active' ) == true ) {
+//   if ( clicked == false ) {
 //     dropdownLists.removeClass( 'active' );
+//   } else {
+//     clicked = false;
 //   }
 // });
 
 dropdownLabels.click( function () {
   var nextElem = $( this ).next();
-  nextElem.toggleClass( 'active' );
+  nextElem.addClass( 'active' );
+  dropdownLists.each( function () {
+    if ( $( this ).hasClass( 'clicked' ) ) {
+      $( this ).removeClass('active');
+      $( this ).removeClass('clicked');
+    } else {
+      $( this ).removeClass( 'clicked' );
+    }
+  });
+  if ( nextElem.hasClass('active') ) {
+    nextElem.toggleClass( 'clicked' );
+  }
 });
